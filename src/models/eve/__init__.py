d795f76c0c3bb5a8010c926731e0db480b2e3c55
Tyron Denker <tyronevedenker@gmail.com>
2025-10-20 09:22:37 +0300

    Add EVE Online data models initialization in __init__.py

---
+++ src/models/eve/__init__.py
@@ -0,0 +1,30 @@
+"""EVE Online data models."""
+
+from .blueprint import EveBlueprint
+from .category import EveCategory
+from .dogma import (
+    EveDogmaAttribute,
+    EveDogmaAttributeCategory,
+    EveDogmaEffect,
+    EveDogmaUnit,
+)
+from .group import EveGroup
+from .market_group import EveMarketGroup
+from .text import EveLocalizedText, EveLocalizedTextRequired
+from .type import EveType
+from .type_material import EveTypeMaterial
+
+__all__ = [
+    "EveBlueprint",
+    "EveCategory",
+    "EveDogmaAttribute",
+    "EveDogmaAttributeCategory",
+    "EveDogmaEffect",
+    "EveDogmaUnit",
+    "EveGroup",
+    "EveLocalizedText",
+    "EveLocalizedTextRequired",
+    "EveMarketGroup",
+    "EveType",
+    "EveTypeMaterial",
+]
