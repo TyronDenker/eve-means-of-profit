636b882b944bc3631500d7a50b343015d9401216
Tyron Denker <tyronevedenker@gmail.com>
2025-10-26 21:12:32 +0200

    Add __init__.py for reusable UI widgets

---
+++ src/ui/widgets/__init__.py
@@ -0,0 +1,13 @@
+"""Reusable UI widgets."""
+
+from .blueprint_viewer import BlueprintViewer
+from .filter_panel import FilterPanel
+from .search_bar import SearchBar
+from .types_browser import TypesBrowser
+
+__all__ = [
+    "BlueprintViewer",
+    "FilterPanel",
+    "SearchBar",
+    "TypesBrowser",
+]
