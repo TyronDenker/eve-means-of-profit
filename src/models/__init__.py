7977df9ae21f810af1f1f1af52d1633231ba8d47
TyronDenker <tyronevedenker@gmail.com>
2025-11-24 16:07:16 +0200

    Add EVE Online data models to export of models

---
+++ src/models/__init__.py
@@ -0,0 +1,39 @@
+"""EVE Online data models (domain layer)."""
+
+from .eve import (
+    EveAsset,
+    EveCategory,
+    EveContract,
+    EveContractItem,
+    EveCorporationProject,
+    EveGroup,
+    EveIndustryJob,
+    EveJournalEntry,
+    EveLocation,
+    EveMarketGroup,
+    EveMarketOrder,
+    EvePosition,
+    EveStation,
+    EveStructure,
+    EveTransaction,
+    EveType,
+)
+
+__all__ = [
+    "EveAsset",
+    "EveCategory",
+    "EveContract",
+    "EveContractItem",
+    "EveCorporationProject",
+    "EveGroup",
+    "EveIndustryJob",
+    "EveJournalEntry",
+    "EveLocation",
+    "EveMarketGroup",
+    "EveMarketOrder",
+    "EvePosition",
+    "EveStation",
+    "EveStructure",
+    "EveTransaction",
+    "EveType",
+]
